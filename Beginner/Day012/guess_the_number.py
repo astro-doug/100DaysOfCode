8e277023e902026f1e29b11479ea7a119b402d0c
Doug Hilton <astro-doug-hilton@gmail.com>
2024-07-23 17:29:37 -0400

    added constants for HARD_GUESS_LIMIT and EASY_GUESS_LIMIT, renamed get_starting_guesses to set_difficulty, and refactored to remove global variable carrying number of guesses remaining

--- Beginner/Day012/guess_the_number.py
+++ Beginner/Day012/guess_the_number.py
@@ -2,53 +2,53 @@ import art
 import os
 import random
 
-guesses: int = 0
+HARD_GUESS_LIMIT = 5
+EASY_GUESS_LIMIT = 10
 
 def cls() -> None:
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
-def get_starting_guesses(mode: str) -> int:
-    global guesses
+def set_difficulty() -> int:
+    global EASY_GUESS_LIMIT
+    global HARD_GUESS_LIMIT
+    mode: str = input("Would you like 'H'ard mode, or 'E'asy mode? ").upper()
+
     if mode == "H":
-        guesses = 5
+        guesses = HARD_GUESS_LIMIT
     else:
-        guesses = 10
+        guesses = EASY_GUESS_LIMIT
+    print(f"{'Hard' if mode == 'H' else 'Easy'} it is! You have {guesses} guesses!")
     return guesses
 
 
 def get_input() -> int:
-    guess:int = int(input("Enter your guess: "))
+    guess: int = int(input("Enter your guess: "))
     return guess
 
 
 def check_guess(guess: int, target: int) -> bool:
-    global guesses
     if guess == target:
         print(art.correct)
         return True
     elif guess < target:
-        guesses -= 1
         print("Too low!")
     elif guess > target:
-        guesses -= 1
         print("Too high!")
 
 
 def play_game() -> None:
-    global guesses
-
     cls()
     print(art.logo)
     target_number: int = random.randint(1, 100)
-    print(f"Target: {target_number}")
+    # print(f"Target: {target_number}")
     print("I'm thinking of a number between 1 and 100.")
     print("         Can you guess my number?")
     print()
-    mode: str = input("Would you like 'H'ard mode, or 'E'asy mode? ").upper()
+    guesses = set_difficulty()
 
-    print(f"{'Hard' if mode == 'H' else 'Easy'} it is! You have {get_starting_guesses(mode)} guesses!")
     while not check_guess(get_input(), target_number):
+        guesses -= 1
         print(f"You have {guesses} guesses remaining")
         if guesses == 0:
             print()
